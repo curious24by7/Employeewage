6326c5b81ed3fef20d528ee9ce7fd5e139096683
sohamdhamnaskar <sohamdham123@gmail.com>
2021-03-25 17:38:41 +0530

    Added: Monthly wages to UC5

--- Empwage.sh
+++ Empwage.sh
@@ -1,20 +1,27 @@
 #!/bin/bash -x
 
-empCheck=$((RANDOM%3))
 isPartTime=1
 isFullTime=2
 empRatePerHr=20
+numWorkingDays=20
+totalSalary=0
 
-case $empCheck in
-	$isPartTime)
-		empHrs=8
-		;;
-	$isFullTime)
-		empHrs=8
-		;;
-	*)
-		empHrs=0
-		;;
-esac
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$((RANDOM%3))
+		case $empCheck in
+			$isPartTime)
+				empHrs=8
+				;;
+			$isFullTime)
+				empHrs=8
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
 
 EmployeeSalary=$(($empHrs*$empRatePerHr))
+totalSalary=$(($totalSalary+$EmployeeSalary))
+
+done
