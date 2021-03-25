bc0977f5c7ac84caf417d957be870c12cf6ac377
sohamdhamnaskar <sohamdham123@gmail.com>
2021-03-25 17:12:00 +0530

    Added: PartTime emp to UC3

--- Empwage.sh
+++ Empwage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 
-empCheck=$((RANDOM%2))
+empCheck=$((RANDOM%3))
 isPresent=1
+isPartTime=2
 EMP_RATE_PER_HR=20
 WORK_HR=8
 
-if [ $empCheck -eq $isPresent ]
+if [ $empCheck -eq $isPresent ] || [ $empCheck -eq $isPartTime ]
 then
    salary=$(($EMP_RATE_PER_HR*$WORK_HR))
 else
