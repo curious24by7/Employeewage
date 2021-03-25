53329e24a9ca90699dd0ac36682a36a2f5a8a0cc
sohamdhamnaskar <sohamdham123@gmail.com>
2021-03-25 16:51:02 +0530

    Added: Employee Attendance to UC1

--- Empwage.sh
+++ Empwage.sh
@@ -1,1 +1,12 @@
-#Welcome to Employee wage computational program
+#!/bin/bash -x
+
+empCheck=$((RANDOM%2))
+isPresent=1
+
+if [ $empCheck -eq $isPresent ]
+then
+echo Employee is Present.
+else
+echo Employee is Absent.
+fi
+
