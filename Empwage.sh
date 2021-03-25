1c50973f4c5e71fb4a2a411f145c1d41b585cc76
sohamdhamnaskar <sohamdham123@gmail.com>
2021-03-25 17:49:36 +0530

    Added: Wages for 100hrs a month to UC6

--- Empwage.sh
+++ Empwage.sh
@@ -1,27 +1,34 @@
 #!/bin/bash -x
 
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
-numWorkingDays=20
-totalSalary=0
+# constants
+IS_FULL_TIME=1
+IS_PART_TIME=2
+EMP_RATE_PER_HR=20
+MAX_HRS_IN_MONTH=100
+NUM_WORKING_DAYS=20
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+# variables
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-		case $empCheck in
-			$isPartTime)
-				empHrs=8
-				;;
-			$isFullTime)
-				empHrs=8
-				;;
-			*)
-				empHrs=0
-				;;
-		esac
+	case $empCheck in
 
-EmployeeSalary=$(($empHrs*$empRatePerHr))
-totalSalary=$(($totalSalary+$EmployeeSalary))
-
+		$IS_FULL_TIME)
+			workHrs=8
+		;;
+		$IS_PART_TIME)
+			workHrs=4
+		;;
+		*)
+			workHrs=0
+		;;
+	esac
+totalEmpHrs=$(($totalEmpHrs+$workHrs))
 done
+	totalSalary=$(($EMP_RATE_PER_HR*$totalEmpHrs))
+	echo $totalSalary
+
